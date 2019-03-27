bfc456b682316eb7d1f9c6a57cd0c6932724d8a4
Bunz <csbiltz08@gmail.com>
2019-03-27 14:52:50 -0400

    Installed Bootstrap

--- html/index.php
+++ html/index.php
@@ -5,28 +5,23 @@ require "includes/header.php";
 ?>
 
        
-        <div class="some-space">
-                <div class="container demo">
+    
 
-                        <div class="content">
-                            
-                           <div id="large-header" class="large-header">
-                              <canvas id="demo-canvas"></canvas>
-                              <p class='pulsing-text'>Space-Dust</p><br><p class='pulsing-text'> Coming Soon</p>
-                              <br>
-                           
-                           </div>
-                        </div>
-                     </div>
-      </div>
 
+
+
+    <h1>Space-Dust</h1>
+    
+    
+
+  
 
    
 
-     <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/499416/TweenLite.min.js"></script>
-     <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/499416/EasePack.min.js"></script>
-     <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/499416/demo.js"></script>
      
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
      
 <?php
 
