fda926d67f9cebbb753523b5d601e4af31ba5d9c
Notare Alname <thisismythirdemail@yahoo.com>
2019-03-27 20:06:54 -0500

    merged login to index

--- html/js/index.js
+++ html/js/index.js
@@ -1,1 +1,9 @@
-
+function toggleLogin() {
+    var element = document.getElementById("login-content");
+    element.classList.toggle("active");
+    if (element.classList.contains("active")) {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
